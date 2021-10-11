831e79d1a277e2f55fd6a46a8782519bf7fc6cdf
Mayuridalavai <mayuri.gj@gmail.com>
2021-10-11 08:20:44 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -16,6 +16,18 @@
 
 // a) Create a test with expect statements using the variables provided.
 
+// describe method that lists the name of the function
+// test method describes what function does
+// an expect method calls the function to test arguments
+
+describe("codeString",() => {
+    it('takes afunction that takes in a string and returns a coded message',() =>{
+    expect(codeString(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+    expect(codeString(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+    expect(codeString(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+    })
+
 var secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
 var secretCodeWord2 = "Gobbledygook"
@@ -27,28 +39,96 @@ var secretCodeWord3 = "Eccentric"
 
 // b) Create the function that makes the test pass.
 
+//declare a function named codeString and take one parameter string
+//split the string using in to array like string.split('') 
+//declate variable named newstr
+//use map for iterating the whole string
+//use value.replace to replace the 'aeio' to '4310' using syntax .replace(/[what you want to replace]/g, 'replace with')
+// join the array as string  using join like newstr.join("") 
+
+const codeString = (string)=>{
+    var string1 = string.split('')
+    let newStr = string1.map(value => {
+        return value = value.replace(/['a']/g,'4'),
+       value = value.replace(/['e','E']/g,'3'),
+       value = value.replace(/['i']/g,'1'),
+       value = value.replace(/['o']/g,'0')
+    })
+       
+    return newStr.join("")
+}
+console.log(codeString(secretCodeWord1))
+console.log(codeString(secretCodeWord2))
+console.log(codeString(secretCodeWord3))
 
 
-// --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
 
-// a) Create a test with expects statement using the variable provided.
 
-var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
-var letterA = "a"
-// Expected output: ["Apple", "Banana", "Orange"]
-var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+
+// // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
+
+// // a) Create a test with expects statement using the variable provided.
+
+// //describe method that lists the name of the function
+// //test method describes what function does
+// //an expect method calls the function to test arguments
+
+describe("singleLetter",() => {
+    it('given an array of words and a single letter and returns all the words that contain that particular letter',() =>{
+    expect(singleLetter(arrayOfWords1,letterA)).toEqual(["Apple", "Banana", "Orange"])
+    expect(singleLetter(arrayOfWords2,letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    
+    })
+    })
+
+ var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+ var letterA = "a"
+ // Expected output: ["Apple", "Banana", "Orange"]
+ var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
 var letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+ // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 
 
 // b) Create the function that makes the test pass.
+
+//declare a function named singleLetter and take to parameters array and string
+//take a variable named newarray 
+//use filter to iterate the array of string 
+//includes is a method performs a case-sensitive search 
+//toUppercase is used to get Uppercase values
+
+const singleLetter = (array,string)=>{
+    let newarray = array.filter(value => { 
+        return value.includes(string) || value.includes(string.toUpperCase())
+      })
+      return newarray
+}
+console.log(singleLetter(arrayOfWords1,letterA))
+console.log(singleLetter(arrayOfWords2,letterE))
+
+
+
+
 
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+
+//describe method that lists the name of the function
+//test method describes what function does
+//an expect method calls the function to test arguments
+
+describe("fullHouse",() => {
+    it('array of five numbers and determines whether or not the array is a “full house”',() =>{
+    expect(fullHouse(hand1)).toEqual(true)
+    expect(fullHouse(hand2)).toEqual(false)
+    expect(fullHouse(hand3)).toEqual(false)
+    
+    })
+    })
 
 var hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -59,4 +139,30 @@ var hand3 = [5, 5, 5, 5, 4]
 
 
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
+
+// declare a function named fullHouse 
+//take variable named count(x) to count the duplicates in array
+//The (for...of) statement iterates over values that the iterable object defines to be iterated over
+//take variable named value count the value in objects
+//conditional statement if/else is used to check the array of values
+
+ const fullHouse = (array) => {
+    let count = {}
+    for(let x of array){
+      count[x] = (count[x] || 0) + 1;
+    }
+    let value = Object.values(count);
+    if((value[0] === 2 && value[1] === 3) || (value[1] === 2 && value[0] === 3)){
+      return true;
+    }else
+    return false;
+}       
+console.log(fullHouse(hand1))
+console.log(fullHouse(hand2))
+console.log(fullHouse(hand3))
+
+
+
+
+
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -6,29 +6,76 @@
 beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
 letter_o = 'o'
-# Expected output: ['coffee', 'soda water']
+# # Expected output: ['coffee', 'soda water']
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
+     
+def one_letter array,letter
+    array.select do |value|
+    value.include?(letter)
+    end
+end           
+p one_letter(beverages_array,letter_t)
+p one_letter(beverages_array,letter_o)
 
 
 # -------------------2) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
 
 nums_array1 = [42, 7, 27]
-# Expected output: 76
-
+# # # Expected output: 76
 nums_array2 = [25, 17, 47, 11]
-# Expected output: 100
+# # Expected output: 100
 
-
+def num_sum array
+     array.sum
+end   
+p num_sum nums_array1
+p num_sum nums_array2
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
-
+class Bike
+    def initialize model_param, wheels_param, current_speed_param
+        @model = model_param
+        @wheel = wheels_param
+        @current_speed = current_speed_param
+    end
+    def get_bike_info
+       p "The #{@model} bike has #{@wheel} wheels and is going #{@current_speed} mph"
+    end
+end
+bike = Bike.new("Trek",2,0)
+bike.get_bike_info
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(10) => 20
 # Expected output example: my_bike.brake(25) => 0
+
+class Bike
+    def initialize(model_param, wheels_param, current_speed_param)
+        @model = model_param
+        @wheels = wheels_param
+        @current_speed = current_speed_param
+    end
+    def get_bike_info
+        p "The #{@model} bike has #{@wheel} wheels and is going #{@current_speed} mph"
+    end
+    def pedal_faster(inc_speed)
+        @current_speed += inc_speed
+    end
+    def brake(dec_speed)
+        if dec_speed > @current_speed
+            @current_speed = 0
+        else
+            @current_speed -= dec_speed
+        end
+    end
+end
+p my_bike = Bike.new("Trek",2,0)
+p my_bike.pedal_faster(10)
+p my_bike.pedal_faster(10)
+p my_bike.brake(25)
